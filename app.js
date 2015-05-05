f4bdebf25fc0dde69e3154bcf36626cf1e698798
Paul Jones <shmiko@gmail.com>
2015-05-06 07:09:29 +1000

    adding post routes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/dashAPI');
 
-var Dash = require('./models/dashModel');
+var Dashboard = require('./models/dashModel');
 
 var app = express();
 
@@ -20,52 +20,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
-var myRouter = express.Router();
 
-myRouter.route('/Dashboards')
-    .post(function(req,res){
-        var dashboard = new Dashboard(req.body);
-        dashboard.save();
-        res.status(201).send(dashboard);
-        //console.log(dashboard);
-
-        //res.send(dashboard);
-    })
-
-    .get(function(req,res){
-        var query = {};
-
-        if (req.query.widgets)
-        {
-            query.widgets = req.query.widgets;
-        }
-        //var responseJSON = {hello: "hello paul"};
-        Dash.find(query, function(err, dashboards){
-            if (err)
-                res.status(500).send(err);
-                //console.log(err);
-            else
-                res.json(dashboards);
-        });
-        //res.json(responseJSON);
-    });
-
-myRouter.route('/Dashboards/:id')
-    .get(function(req,res){
-
-        //var responseJSON = {hello: "hello paul"};
-        Dash.findById(req.params.id, function(err, dashboard){
-            if (err)
-                res.status(500).send(err);
-            //console.log(err);
-            else
-                res.json(dashboard);
-        });
-        //res.json(responseJSON);
-    });
+dashboardRouter = require('./routes/dashboardRoutes')(Dashboard);
 
 
-app.use('/api', myRouter);
+app.use('/api', dashboardRouter);
 
 app.get('/', function(req,res){
     res.send('Gotcha');
---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,56 @@
+/**
+ * Created by pauljones on 6/05/15.
+ */
+var express = require('express');
+
+var routes = function(Dashboard){
+    var dashboardRouter = express.Router();
+
+    dashboardRouter.route('/Dashboards')
+        .post(function(req,res){
+            var dashboard = new Dashboard(req.body);
+            dashboard.save();
+            res.status(201).send(dashboard);
+            //console.log(dashboard);
+
+            //res.send(dashboard);
+        })
+
+        .get(function(req,res){
+            var query = {};
+
+            if (req.query.widgets)
+            {
+                query.widgets = req.query.widgets;
+            }
+            //var responseJSON = {hello: "hello paul"};
+            Dashboard.find(query, function(err, dashboards){
+                if (err)
+                    res.status(500).send(err);
+                //console.log(err);
+                else
+                    res.json(dashboards);
+            });
+            //res.json(responseJSON);
+        });
+
+    dashboardRouter.route('/Dashboards/:id')
+        .get(function(req,res){
+
+            //var responseJSON = {hello: "hello paul"};
+            Dashboard.findById(req.params.id, function(err, dashboard){
+                if (err)
+                    res.status(500).send(err);
+                //console.log(err);
+                else
+                    res.json(dashboard);
+            });
+            //res.json(responseJSON);
+        });
+    return dashboardRouter;
+};
+
+module.exports = routes;
+
+
+
