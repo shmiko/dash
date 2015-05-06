daad3f535e82995113554c701d997028160188d8
Paul Jones <shmiko@gmail.com>
2015-05-07 07:41:55 +1000

    added tests working

--- Tests/dashboardControllerTests.js
+++ Tests/dashboardControllerTests.js
@@ -3,3 +3,29 @@
  */
 var should = require('should');
 var sinon = require('sinon');
+
+describe('Dashboard Controller Tests:', function(){
+    describe('Post', function(){
+        it('should not allow an empty title on post', function(){
+            var Dashboard = function(book){this.save = function(){}};
+
+            var req = {
+                body: {
+                    pageTitle: 'Join'
+                }
+            }
+
+            var res = {
+                status: sinon.spy(),
+                send: sinon.spy()
+            }
+
+            var dashboardController = require('../controllers/dashboardController')(Dashboard);
+
+            dashboardController.post(req,res);
+
+            res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0][0]);
+            res.send.calledWith('Title is required').should.equal(true);
+        })
+    })
+})
