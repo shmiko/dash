5e3771408829de1fd4a3befa3368df77eff190f3
Paul Jones <shmiko@gmail.com>
2015-05-07 06:55:30 +1000

    added route use, but lost id

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -10,6 +10,12 @@ var routes = function(Dashboard){
     var dashboardRouter = express.Router();
 
     dashboardRouter.route('/Dashboards')
+        .post(function(req,res){
+            var dashboard = new Dashboard(req.body);
+            dashboard.save();
+            res.status(201).send(dashboard);
+            //console.log(dashboard);
+        })
         .get(function(req,res){
 
             var query = {};
@@ -25,18 +31,84 @@ var routes = function(Dashboard){
                     res.json(dashboards);
             });
         });
-
-    dashboardRouter.route('/Dashboards/:id')
+    dashboardRouter.use('/:dashboardId', function(req,res,next){
+        Dashboard.findById(req.params.id, function(err,dashboard){
+            console.log(req.params.id);
+            if(err)
+                res.status(500).send(err);
+            else if(dashboard)
+            {
+                req.dashboard = dashboard;
+                next();
+            }
+            else
+            {
+                res.status(404).send('no dashboard found');
+            }
+        });
+    });
+    dashboardRouter.route('/:dashboardId')
         .get(function(req,res){
 
+            res.json(req.dashboard);
 
-            Dashboard.findById(req.params.id, function(err,dashboard){
+        })
+        .put(function(req,res){
+            req.dashboard.pageTitle = req.body.pageTitle;
+            req.dashboard.widgets = req.body.widgets;
+            req.dashboard.saveDash = req.body.saveDash;
+            req.dashboard.save(function(err){
                 if(err)
                     res.status(500).send(err);
-                else
-                    res.json(dashboard);
+                else{
+                    res.json(req.dashboard);
+                }
+            });
+        })
+        .patch(function(req,res){
+            if(req.body._id)
+                delete req.body._id;
+
+            for(var p in req.body)
+            {
+                req.dashboard[p] = req.body[p];
+            }
+
+            req.dashboard.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.dashboard);
+                }
+            });
+        })
+        .delete(function(req,res){
+            req.dashboard.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.status(204).send('Removed');
+                }
             });
         });
+    //dashboardRouter.route('/Dashboards/:id')
+    //    .get(function(req,res){
+    //
+    //
+    //        Dashboard.findById(req.params.id, function(err,dashboard){
+    //            if(err)
+    //                res.status(500).send(err);
+    //            else if(dashboard)
+    //            {
+    //                req.dashboard = dashboard;
+    //                next();
+    //            }
+    //            else
+    //            {
+    //                res.status(404).send('no dashboard found');
+    //            }
+    //        });
+    //    });
 
     return dashboardRouter;
 };
