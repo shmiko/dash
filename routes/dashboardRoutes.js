5b114f94755339cfe38c67abec45cb36533b90cf
Paul Jones <shmiko@gmail.com>
2015-05-06 19:47:32 +1000

    adding post routes, new routes file

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -12,16 +12,14 @@ var routes = function(Dashboard){
             dashboard.save();
             res.status(201).send(dashboard);
             //console.log(dashboard);
-
-            //res.send(dashboard);
         })
 
         .get(function(req,res){
             var query = {};
 
-            if (req.query.widgets)
+            if (req.query.pageTitle)
             {
-                query.widgets = req.query.widgets;
+                query.pageTitle = req.query.pageTitle;
             }
             //var responseJSON = {hello: "hello paul"};
             Dashboard.find(query, function(err, dashboards){
@@ -33,19 +31,66 @@ var routes = function(Dashboard){
             });
             //res.json(responseJSON);
         });
+    dashboardRouter.use('/:dashboardId', function(req,res,next){
+        Dashboard.findById(req.params.id, function(err,dashboard){
+            console.log(req.params.id);
+            if(err)
+                res.status(500).send(err);
+            else if(dashboard)
+            {
+                req.dashboard = dashboard;
+                next();
+            }
+            else
+            {
+                res.status(404).send('no dashboard found');
+            }
+        });
+    });
 
-    dashboardRouter.route('/:id')
+    dashboardRouter.route('/:dashboardId')
         .get(function(req,res){
 
-            //var responseJSON = {hello: "hello paul"};
-            Dashboard.findById(req.params.id, function(err, dashboard){
-                if (err)
+            res.json(req.dashboard);
+
+        })
+        .put(function(req,res){
+            req.dashboard.pageTitle = req.body.pageTitle;
+            req.dashboard.widgets = req.body.widgets;
+            req.dashboard.saveDash = req.body.saveDash;
+            req.dashboard.save(function(err){
+                if(err)
                     res.status(500).send(err);
-                //console.log(err);
-                else
-                    res.json(dashboard);
+                else{
+                    res.json(req.dashboard);
+                }
             });
-            //res.json(responseJSON);
+        })
+        .patch(function(req,res){
+            if(req.body._id)
+                delete req.body._id;
+
+            for(var p in req.body)
+            {
+                req.dashboard[p] = req.body[p];
+            }
+
+            req.dashboard.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.dashboard);
+                }
+            });
+        })
+        .delete(function(req,res){
+            req.dashboard.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.status(204).send('Removed');
+                }
+            });
         });
     return dashboardRouter;
 };
