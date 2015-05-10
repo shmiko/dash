3a59ae44efc4e91efd19cee95621405d64dbf6a6
Paul Jones <shmiko@gmail.com>
2015-05-10 21:40:40 +1000

    adding google maps

--- app/directives/wwaMapsDirective.js
+++ app/directives/wwaMapsDirective.js
@@ -4,6 +4,67 @@ angular.module('app').directive('wwaMaps', [function () {
     return {
         scope: {
         },
-        template: '<h1>Maps Page</h1>'
+        //template: '<h1>Maps Page</h1>'
+        templateUrl: 'app/map.html',
+        link: function (scope) {
+
+           // var vm = this;
+
+            scope.activate = activate;
+            //vm.location = initialData;
+            scope.refreshMap = refreshMap;
+            //vm.save = save;
+            scope.title = 'Maps';//($stateParams.id ? 'Edit Location': 'Add Location');
+
+            scope.map = {
+                center: {
+                    latitude: currentPosition.coords.latitude,
+                    longitude: currentPosition.coords.longitude
+                },
+                zoom: 12
+            };
+
+            scope.marker = {
+                id: 1,
+                coords: currentPosition.coords
+            };
+
+            activate();
+
+            ////////////////
+
+            function activate() {
+                if (scope.location.address){
+                    refreshMap();
+                }
+            }
+
+            function refreshMap(){
+                var geocoder = new maps.Geocoder();
+                geocoder.geocode({ address: scope.location.address }, function(result){
+                    if (result.length > 0) {
+                        var addrLocation = result[0].geometry.location;
+
+                        $timeout(function(){
+                            scope.map.center = {
+                                latitude: addrLocation.lat(),
+                                longitude: addrLocation.lng()
+                            };
+
+                            scope.marker = {
+                                id: 1,
+                                coords: {
+                                    latitude: scope.map.center.latitude,
+                                    longitude: scope.map.center.longitude
+                                },
+                                options: {
+                                    title: scope.location.name
+                                }
+                            };
+                        }, 0);
+                    }
+                });
+            }
+        }
     }
 }]);
