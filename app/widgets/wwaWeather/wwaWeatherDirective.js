76ee1cd1ada0a423e767d41d2ed362900f88fcf9
Paul Jones <shmiko@gmail.com>
2015-05-19 09:11:45 +1000

    weather widget almost working

--- app/widgets/wwaWeather/wwaWeatherDirective.js
+++ app/widgets/wwaWeather/wwaWeatherDirective.js
@@ -1,78 +1,98 @@
-/**
- * Created by pauljones on 19/05/15.
- */
-(function() {
+﻿(function() {
 
-"use strict";
+    "use strict";
 
-var app = angular.module('app');// DIRECTIVES
+    var app = angular.module('app');
 
-app.directive("wwaWeather", function(dataService) {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/widgets/wwaWeather/wwaWeatherTemplate.html',
-        replace: true,
-        scope: {
-            weatherDay: "=",
-            convertToStandard: "&",
-            convertToDate: "&",
-            dateFormat: "@"
-        },
-        link: function (scope, el, attrs) {
-            scope.isLoaded = false;
-            scope.hasError = false;
-            scope.selectedLocation = null;
+    app.directive('wwaWeather', ['dataService','weatherService',
+        function (dataService,weatherService) {
+            var weatherController = function($scope,$routeParams,$resource) {
+                var vm = this;
+                //city = weatherService.city;
+                //
+                //city.$watch('city', function() {
+                //    cityService.city = $scope.city;
+                //});
+                vm.city = weatherService.city;
 
-            scope.loadlocation = function () {
-                scope.hasError = false;
-                dataService.getLocation(scope.item.widgetSettings.id)
-                    .then(function (data) {
-                        //success
-                        scope.selectedLocation = data;
-                        scope.isLoaded = true;
-                        scope.hasError = false;
-                    }, function (data) {
-                        //error
-                        scope.hasError = true;
-                    });
+                //$scope.$watch('city', function() {
+                //    weatherService.city = vm.city;
+                //});
+
+                vm.days = $routeParams.days || '2';
+
+                vm.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+                vm.weatherResult = vm.weatherAPI.get({ q: vm.city, cnt: vm.days });
+
+                vm.convertToFahrenheit = function(degK) {
+
+                    return Math.round((1.8 * (degK - 273)) + 32);
+
+                };
+
+                vm.convertToDate = function(dt) {
+
+                    return new Date(dt * 1000);
+
+                };
+
+                vm.weatherDay = "=";
+                vm.convertToStandard = "&";
+                vm.convertToDate = "&";
+                vm.dateFormat = "@";
             };
 
-            scope.loadlocation();
-        }
-    }
-})
-    .controller('weatherController', ['$scope', 'weatherService', function($scope, cityService) {
+            //var forecastController = function(weatherService) {
+            //    $scope.city = weatherService.city;
+            //
+            //    $scope.days = $routeParams.days || '2';
+            //
+            //    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+            //
+            //    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+            //
+            //    $scope.convertToFahrenheit = function(degK) {
+            //
+            //        return Math.round((1.8 * (degK - 273)) + 32);
+            //
+            //    };
+            //
+            //    $scope.convertToDate = function(dt) {
+            //
+            //        return new Date(dt * 1000);
+            //
+            //    };
+            //
+            //};
 
-        $scope.city = cityService.city;
+            return {
+                controller: weatherController,
+                controllerAs: 'vm',
+                bindToController: true,
+                templateUrl: 'app/widgets/wwaWeather/wwaWeatherTemplate.html',
+                link: function (scope, el, attrs) {
+                    scope.isLoaded = false;
+                    scope.hasError = false;
+                    scope.selectedLocation = null;
 
-        $scope.$watch('city', function() {
-            cityService.city = $scope.city;
-        });
+                    scope.loadlocation = function () {
+                        scope.hasError = false;
+                        dataService.getLocation(scope.item.widgetSettings.id)
+                        .then(function(data){
+                            //success
+                           scope.selectedLocation = data;
+                           scope.isLoaded = true;
+                           scope.hasError = false;
+                        }, function (data) {
+                            //error
+                            scope.hasError = true;
+                        });
+                    };
 
-    }])
-
-    .controller('forecastController', ['$scope', '$resource', '$routeParams', 'weatherService', function($scope, $resource, $routeParams, weatherService) {
-
-    $scope.city = weatherService.city;
-
-    $scope.days = $routeParams.days || '2';
-
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
-
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
-
-    $scope.convertToFahrenheit = function(degK) {
-
-        return Math.round((1.8 * (degK - 273)) + 32);
-
-    };
-
-    $scope.convertToDate = function(dt) {
-
-        return new Date(dt * 1000);
-
-    };
-
-}]);
+                    scope.loadlocation();
+                }
+            };
+    }]);
 
 } () );
