a61f79d0ce19c63ecdeb5fafda1df527bb58168f
Paul Jones <shmiko@gmail.com>
2015-05-18 21:38:22 +1000

    css changes

--- app/appController.js
+++ app/appController.js
@@ -4,7 +4,7 @@ angular.module('app').controller('appController',
 	[
 	'$scope',
 		function ($scope) {
-			$scope.state = 'unauthorised';
+			$scope.state = 'authorised';
 			$scope.signIn = function () {
 				$scope.state = 'authorised';
 			};
