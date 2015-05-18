16cb296739b21356774c3cf2e54c338bad334934
Paul Jones <shmiko@gmail.com>
2015-05-18 11:40:34 +1000

    added iife to temp directive

--- app/widgets/wwaTemperature/wwaTemperatureDirective.js
+++ app/widgets/wwaTemperature/wwaTemperatureDirective.js
@@ -1,33 +1,39 @@
-﻿"use strict";
+﻿(function() {
 
-angular.module('app')
+    "use strict";
 
-    .directive('wwaTemperature',
-    ['dataService',
-    function (dataService) {
-        return {
-            templateUrl: 'app/widgets/wwaTemperature/wwaTemperatureTemplate.html',
-            //controller: 'temperatureWidgetController',
-            link: function (scope, el, attrs) {
-                scope.isLoaded = false;
-                scope.hasError = false;
-                scope.selectedLocation = null;
-                
-                scope.loadlocation = function () {
+
+
+    angular.module('app')
+
+        .directive('wwaTemperature',
+        ['dataService',
+        function (dataService) {
+            return {
+                templateUrl: 'app/widgets/wwaTemperature/wwaTemperatureTemplate.html',
+                //controller: 'temperatureWidgetController',
+                link: function (scope, el, attrs) {
+                    scope.isLoaded = false;
                     scope.hasError = false;
-                    dataService.getLocation(scope.item.widgetSettings.id)
-                    .then(function(data){
-                        //success
-                       scope.selectedLocation = data;
-                       scope.isLoaded = true;
-                       scope.hasError = false;
-                    }, function (data) {
-                        //error
-                        scope.hasError = true;
-                    });
-                };
+                    scope.selectedLocation = null;
 
-                scope.loadlocation();
-            }
-        };
-}]);
+                    scope.loadlocation = function () {
+                        scope.hasError = false;
+                        dataService.getLocation(scope.item.widgetSettings.id)
+                        .then(function(data){
+                            //success
+                           scope.selectedLocation = data;
+                           scope.isLoaded = true;
+                           scope.hasError = false;
+                        }, function (data) {
+                            //error
+                            scope.hasError = true;
+                        });
+                    };
+
+                    scope.loadlocation();
+                }
+            };
+    }]);
+
+} () );
