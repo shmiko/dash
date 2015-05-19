d9101d215128a3b7a65d67ed0b5e58c0b290361e
Paul Jones <shmiko@gmail.com>
2015-05-19 19:20:11 +1000

    server.js working

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-// modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+// Require Modules
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-// configuration ===========================================
+// Array of Mime Types
+var mimeTypes = {
+    "html" : "text/html",
+    "jpeg" : "image/jpeg",
+    "jpg" : "image/jpeg",
+    "png" : "image/png",
+    "js" : "text/javascript",
+    "css" : "text/css"
+};
 
-// config files
-var db = require('./config/db');
+// Create Server
+http.createServer(function(req, res){
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(),unescape(uri));
+    console.log('Loading '+ uri);
+    var stats;
 
-// set our port
-var port = process.env.PORT || 8080;
+    try{
+        stats = fs.lstatSync(fileName);
+    } catch(e) {
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
 
-// connect to our mongoDB database 
-// (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
+    // Check if file/directory
+    if(stats.isFile()){
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-type': mimeType});
 
-// get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json());
-
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
-
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/app'));
-
-//app.get('*', function(req, res) {
-//    res.sendFile('indexO.html'); // load our public/index.html file
-//});
-
-// routes ==================================================
-//require('appModule')(app); // configure our routes
-
-// start app ===============================================
-// startup our app at http://localhost:8080
-app.listen(port);
-
-// shoutout to the user                     
-console.log('Magic happens on port ' + port);
-
-// expose app           
-exports = module.exports = app;                         
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()){
+        res.writeHead(302,{
+            'Location' : 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-Type' : 'text/plain'});
+        res.write('500 Internal Error\n');
+        res.end();
+    }
+}).listen(8080);
