bb02918264a841a8ff692c426a158538379b7f9e
Paul Jones <shmiko@gmail.com>
2015-07-19 16:59:56 +1000

    layout, sticky footer, fixed menu sizing when hori, added media qry for testing menu sizing

--- app/directives/wwaDashboardDirective.js
+++ app/directives/wwaDashboardDirective.js
@@ -11,6 +11,15 @@ angular.module('app')
 
             scope.title = "TripStomp Smashboard";
 
+            //scope.gridsterOpts = {
+            //    columns: 12,
+            //    margins: [50, 50],
+            //    outerMargin: false,
+            //    pushing: true,
+            //    floating: true,
+            //    swapping: true
+            //};
+
             scope.gridsterOpts = {
                 //columns: 6,
                 //margins: [20, 20],
@@ -21,38 +30,38 @@ angular.module('app')
                 columns: 12, // the width of the grid, in columns
                 pushing: true, // whether to push other items out of the way on move or resize
                 floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
-                swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
+                swapping: true, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
                 width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
                 colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
                 rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
-                margins: [50, 50], // the pixel distance between each widget
+                margins: [1, 12], // the pixel distance between each widget
                 outerMargin: false, // whether margins apply to outer edges of the grid
                 isMobile: true, // stacks the grid items if true
                 mobileBreakPoint: 3600, // if the screen is not wider that this, remove the grid layout and stack the items
-                mobileModeEnabled: true, // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
-                minColumns: 1, // the minimum columns the grid must have
-                minRows: 2, // the minimum height of the grid, in rows
-                maxRows: 100,
-                defaultSizeX: 4, // the default width of a gridster item, if not specifed
-                defaultSizeY: 2, // the default height of a gridster item, if not specified
-                minSizeX: 1, // minimum column width of an item
-                maxSizeX: null, // maximum column width of an item
-                minSizeY: 1, // minumum row height of an item
-                maxSizeY: null, // maximum row height of an item
-                resizable: {
-                    enabled: true,
-                    handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
-                    start: function(event, $element, widget) {}, // optional callback fired when resize is started,
-                    resize: function(event, $element, widget) {}, // optional callback fired when item is resized,
-                    stop: function(event, $element, widget) {} // optional callback fired when item is finished resizing
-                },
-                draggable: {
-                    enabled: true // whether dragging items is supported
-                    //handle: '.my-class', // optional selector for resize handle
-                    //start: function(event, $element, widget) {}, // optional callback fired when drag is started,
-                    //drag: function(event, $element, widget) {}, // optional callback fired when item is moved,
-                    //stop: function(event, $element, widget) {} // optional callback fired when item is finished dragging
-                }
+                mobileModeEnabled: true // whether or not to toggle mobile mode when screen width is less than mobileBreakPoint
+                //minColumns: 1, // the minimum columns the grid must have
+                //minRows: 2, // the minimum height of the grid, in rows
+                //maxRows: 100,
+                //defaultSizeX: 4, // the default width of a gridster item, if not specifed
+                //defaultSizeY: 2, // the default height of a gridster item, if not specified
+                //minSizeX: 1, // minimum column width of an item
+                //maxSizeX: null, // maximum column width of an item
+                //minSizeY: 1, // minumum row height of an item
+                //maxSizeY: null, // maximum row height of an item
+                //resizable: {
+                //    enabled: true,
+                //    handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
+                //    start: function(event, $element, widget) {}, // optional callback fired when resize is started,
+                //    resize: function(event, $element, widget) {}, // optional callback fired when item is resized,
+                //    stop: function(event, $element, widget) {} // optional callback fired when item is finished resizing
+                //},
+                //draggable: {
+                //    enabled: true, // whether dragging items is supported
+                //    handle: '.my-class', // optional selector for resize handle
+                //    start: function(event, $element, widget) {}, // optional callback fired when drag is started,
+                //    drag: function(event, $element, widget) {}, // optional callback fired when item is moved,
+                //    stop: function(event, $element, widget) {} // optional callback fired when item is finished dragging
+                //}
             };
 
             scope.widgetDefinitions = [
