bb319c4911f2259094adc6c35dadd8c10ca14825
Paul Jones <shmiko@gmail.com>
2015-09-14 08:04:28 +1000

    refresh..

--- tripstomp/www/js/app.js
+++ tripstomp/www/js/app.js
@@ -77,14 +77,14 @@ angular.module('starter', ['ionic'])
 
       $scope.onItemDelete = function(dayIndex, item) {
         $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
-      }
+      };
 
       $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
           $scope.calendar = data.calendar;
           $scope.$broadcast('scroll.refreshComplete');
         });
-      }
+      };
 
       $scope.toggleStar = function(item) {
         item.star = !item.star;
@@ -107,13 +107,13 @@ angular.module('starter', ['ionic'])
       $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
           $scope.artists = data;
-          $scope.$broadcast('scroll.refreshComplete'); 
+          $scope.$broadcast('scroll.refreshComplete');
         });
-      }
+      };
 
       $scope.toggleStar = function(item) {
         item.star = !item.star;
-      }
+      };
 
       $scope.moveItem = function(item, fromIndex, toIndex) {
         $scope.artists.splice(fromIndex, 1);
